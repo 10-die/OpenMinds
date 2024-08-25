245df4d617c0550b4211009cf5f995d0c358d2d9
10die <161975861+10-die@users.noreply.github.com>
2024-08-26 01:50:21 +0200

    installed supabase and creates env variable and subabaseClient.ts

--- src/app/components/contactUsBox/contactUsBox.tsx
+++ src/app/components/contactUsBox/contactUsBox.tsx
@@ -8,17 +8,22 @@ const ContactUsBox = () =>
         <div className={styles.mainContainer}>
             <div className={styles.midContainerOne}>
             <input
-                name="name"
-                placeholder="Your Name"
+                name="firstName"
+                placeholder="First Name"
+                required
+                />
+                <input
+                name="lastName"
+                placeholder="Last Name"
                 required
                 />
             <input
                 name="email"
-                placeholder="Your Email"
+                placeholder="Email"
                 required
                 />
             <textarea
-                placeholder="Your Message"
+                placeholder="Message"
                 />
             <button type="button">
                 Send
--- src/app/pages/contactUs/page.tsx
+++ src/app/pages/contactUs/page.tsx
@@ -1,11 +1,17 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import TopBanner from "../../components/topBanner/TopBanner"
 import ContactUsBox from "../../components/contactUsBox/ContactUsBox";
 import styles from "./page.module.css";
+import { supabase } from "@/"
 
 export default function contactUs()
 {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
     return(
             <main className={styles.mainContainer}>
                 <div>
